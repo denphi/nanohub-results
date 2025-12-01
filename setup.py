4f7d4698ea9ebcb48c6533c862d9759449b44abc
Daniel Mejia <denphi@denphi.com>
2025-12-01 13:34:23 -0500

    Fix read the docs

--- setup.py
+++ setup.py
@@ -73,7 +73,6 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Science/Research',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
